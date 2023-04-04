56e4bd4532b9413eefebc8098b4b5c3f92c5be51
user <email>
2023-04-05 04:49:40 +0700

    changed names of gifs, update gif.js

--- commands/fun/gif.js
+++ commands/fun/gif.js
@@ -1,13 +1,32 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require('discord.js');
+const fs = require('node:fs');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('gif')
-		.setDescription('Send gif')
+		.setDescription('Отправить гифку')
 		.addStringOption(option =>
-			option.setName('input')
-				.setDescription('The input to echo back')),
+			option
+				.setName('название_гифки')
+				.setDescription('gif')
+				.setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply('gif !');
+		let gifName = interaction.options.getString('название_гифки', true);
+		let gif = new AttachmentBuilder(`./gifs/${gifName}.gif`);
+
+		if (!fs.existsSync(gif.attachment)) {
+			const randomGifNames = ['noclue', 'markmidunderstand', 'vega'];
+			const idx = Math.floor((Math.random() * randomGifNames.length));
+			gifName = randomGifNames[idx];
+			gif = new AttachmentBuilder(`./gifs/${gifName}.gif`);
+		}
+
+		const gifEmbed = new EmbedBuilder()
+			.setAuthor({ name: interaction.member.user.username, iconURL: interaction.member.user.avatarURL() })
+			.setColor(0xca5ad6)
+			.setImage(`attachment://${gifName}.gif`)
+			.setFooter({ text: gifName });
+		const message = await interaction.reply({ embeds: [gifEmbed], files: [gif], fetchReply: true });
+		message.react('😄');
 	},
 };
