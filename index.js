7de5f3219bc62c3a482ac2a6a68a72d97a5c40b3
SergeiMironenko <90122255+SergeiMironenko@users.noreply.github.com>
2023-04-29 16:16:35 +0700

    add landscapes a day and girls a week

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// const got = require('got');
 // Require the necessary discord.js classes
 const fs = require('node:fs');
 const path = require('node:path');
@@ -7,7 +8,11 @@ const { token } = require('./config.json');
 const { EmbedBuilder } = require('discord.js');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-var request = require('request');
+// var request = require('request');
+// const https = require('https');
+// import axios, { isCancel, AxiosError } from 'axios';
+const { request } = require('undici');
+
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -47,69 +52,72 @@ for (const file of eventFiles) {
 	}
 }
 
-var parseMyAwesomeHtml = function (html) {
-	const dom = new JSDOM(html);
-	const img = dom.window.document
-		.querySelector('.wallpaper__item')
-		.querySelector('img')
-		.getAttribute('src');
-	return img;
-};
+async function sendImgFromGoodfon(url, channelId, n) {
+	let files = [];
+	const channel = client.channels.cache.find(channel => channel.id === channelId);
+	const res = await request(url);
+	const body = await res.body.text();
 
-var sendDayPic = function () {
-	const channel = client.channels.cache.find(channel => channel.id === '1033010362913652892');
-	const url = 'https://www.goodfon.ru/catalog/landscapes/';
-	request(url, function (err, res, body) {
-		if (err) throw err;
+	const dom = new JSDOM(body);
+	const elements = dom.window.document
+		.querySelectorAll('.wallpapers__item__wall');
 
-		const dom = new JSDOM(body);
-		const url2 = dom.window.document
-			.querySelector('.wallpapers__item__wall')
+	for (let i = 0; i < n; i++) {
+		const url2 = elements[i]
 			.querySelector('a')
 			.getAttribute('href');
-		console.log(url2);
+		const res2 = await request(url2);
+		const body2 = await res2.body.text();
 
-		request(url2, async function (err, res, body) {
-			if (err) throw err;
+		const dom2 = new JSDOM(body2);
+		const img = dom2.window.document
+			.querySelector('.wallpaper__item')
+			.querySelector('img')
+			.getAttribute('src');
+		files.push({ attachment: img });
+	}
 
-			const img = parseMyAwesomeHtml(body);
-			// const gifEmbed = new EmbedBuilder()
-			// 	.setAuthor({ name: 'ту ту ту ру...ту ту ру ру...' })
-			// 	.setColor(0xca5ad6)
-			// 	.setImage(img);
-			channel.send({
-				files: [{ attachment: img }]
-			});//embeds: [gifEmbed] });
-		});
-	});
+	channel.send({ files: files });
 }
 
 client.on('ready', () => {
 	const t = new Date();
+	const d = t.getDay();
 	const h = t.getHours();
 	const m = t.getMinutes();
 
+	// Время для пейзажей
 	const hTarget = (10 + 7) % 24;
 	const mTarget = 0;
-
+	37
 	const tRemains = (hTarget * 60 + mTarget - (h * 60 + m) + 24 * 60) % (24 * 60);
 	const hRemains = Math.floor(tRemains / 60);
 	const mRemains = tRemains % 60;
 
-	console.log(h, m);
-	console.log(tRemains, hRemains, mRemains);
+	// Время для девушек
+	const dTarget2 = 6;  // Суббота
+	const hTarget2 = (9 + 7) % 24;
+	const mTarget2 = 0;
 
-	// const channel = client.channels.cache.find(channel => channel.id === '1033010362913652892');
-	// channel.send({
-	// 	files: [{ attachment: 'gifs/gena.gif' }, { attachment: 'gifs/gena.gif' }
-	// 		, { attachment: 'gifs/gena.gif' }, { attachment: 'gifs/gena.gif' }, { attachment: 'gifs/gena.gif' }]
-	// });
+	const tRemains2 = (hTarget2 * 60 + mTarget2 - (h * 60 + m) + 24 * 60) % (24 * 60);
+	const dRemains2 = (d - dTarget2 + 7) % 7;
+	const hRemains2 = Math.floor(tRemains2 / 60);
+	const mRemains2 = tRemains2 % 60;
 
+	// console.log(h, m);
+	// console.log(tRemains, hRemains, mRemains);
+
+	// Пейзажи раз в день
 	setTimeout(() => {
-		sendDayPic();
-		setInterval(() => { sendDayPic() }, 1000 * 60 * 60 * 24);
-	}, 1000 * 60 * mRemains + 1000 * 60 * 60 * hRemains)
+		sendImgFromGoodfon('https://www.goodfon.ru/catalog/landscapes/', '1033010362913652892', 4);
+		setInterval(() => { sendImgFromGoodfon() }, 1000 * 60 * 60 * 24);
+	}, 1000 * 60 * mRemains + 1000 * 60 * 60 * hRemains);
 
+	// Девушки раз в неделю
+	setTimeout(() => {
+		sendImgFromGoodfon('https://www.goodfon.ru/catalog/girls/', '1033010362913652892', 3);
+		setInterval(() => { sendImgFromGoodfon() }, 1000 * 60 * 60 * 24 * 7);
+	}, 1000 * 60 * mRemains2 + 1000 * 60 * 60 * hRemains2 + 1000 * 60 * 60 * 24 * dRemains2);
 });
 
 // Log in to Discord with your client's token
